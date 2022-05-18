ede7cf703afe01bcb0be48256b1880b28706c93b
Zeljko Bozic <boziczp@gmail.com>
2022-05-18 16:25:36 +0200

    Flickr image fetched

--- components/counter/counterAPI.js
+++ components/counter/counterAPI.js
@@ -4,3 +4,18 @@ export function fetchCount(amount = 1) {
     setTimeout(() => resolve({ data: amount }), 500)
   );
 }
+
+// TODO: pass page number, tag
+
+const flickrURL = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9077ed96f15f70a86798aca3402a9f5a&tags=cats&extras=owner_name%2C+date_taken%2C+description&page=44&format=json&nojsoncallback=1&auth_token=72157720843817160-d931fd5430d42d37&api_sig=8aac62cae914a1b9fd4b11d0ee7eabf3"
+
+export const getFlickrImages = async () => {
+  try {
+    const response = await fetch(flickrURL);
+    const json = await response.json();
+    console.log('### flickr data ', json.photos)
+    return json.photos;
+  } catch (error) {
+    console.error(error);
+  }
+};
