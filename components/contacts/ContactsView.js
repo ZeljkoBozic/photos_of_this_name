19a2080055fe1ca9eec684cc949a989490343294
Zeljko Bozic <boziczp@gmail.com>
2022-05-22 17:22:51 +0200

    Cleanup contacts main view

--- components/contacts/ContactsView.js
+++ components/contacts/ContactsView.js
@@ -1,22 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import ContactsList from './ContactsList';
-import Contacts from 'react-native-contacts';
-import {StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
 const ContactsView = ({navigation}) => {
-  const [contacts, setContacts] = useState([]);
-
-  useEffect(() => {
-    Contacts.getAll().then(contacts => {
-      setContacts(contacts);
-    });
-  }, []);
-
   return <ContactsList navigation={navigation} />;
 };
-
-const styles = StyleSheet.create({});
 
 ContactsView.propTypes = {
   navigation: PropTypes.object.isRequired,
